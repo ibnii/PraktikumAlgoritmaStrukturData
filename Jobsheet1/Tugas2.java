7b7470d3447bf30f5f0c39e305bb88c6d1cab839
Ibnii <ibniandarta9999@gmail.com>
2026-02-10 13:05:57 +0700

    Fix Tugas 2

--- Jobsheet1/Tugas2.java
+++ Jobsheet1/Tugas2.java
@@ -4,14 +4,41 @@ import java.util.Scanner;
 public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int pilih;
         System.out.print("Masukkan jumlah jadwal: ");
         int jumlahJadwal = sc.nextInt();
         sc.nextLine();
         String[][] jadwal = new String[jumlahJadwal][4];
         inputJadwal(jadwal, sc);
 
-        System.out.println("\n--- Data Masuk ---");
-        tampilkanJadwal(jadwal);
+        do {
+            System.out.println("\n1. Tampilkan Jadwal");
+            System.out.println("2. Tampilkan Berdasar Hari");
+            System.out.println("3. Tampilkan Berdasar MK");
+            System.out.println("0. Keluar");
+
+            System.out.print("Masukkan pilihan : ");
+            pilih = sc.nextInt();
+
+            switch (pilih) {
+                case 1:
+                    tampilkanJadwal(jadwal);
+                    break;
+                case 2:
+                    tampilkanBerdasarHari(jadwal, sc);
+                    break;
+                case 3:
+                    tampilkanBerdasarMK(jadwal, sc);
+                    break;
+                case 0:
+                    System.out.println("Program selesai.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+
+        } while (pilih != 0);
+
     }
 
     static void inputJadwal(String[][] jadwal, Scanner sc) {
@@ -31,12 +58,14 @@ public class Tugas2 {
 
             System.out.println();
         }
+
+        System.out.println("Jadwal kuliah selesai di input");
     }
 
     static void tampilkanJadwal(String[][] jadwal) {
         System.out.println("\n=== JADWAL KULIAH ===");
         System.out.println("---------------------------------------------------------------------");
-        System.out.printf("| %-30s | %-15s | %-10s | %-8s |\n","Mata Kuliah", "Tempat", "Hari", "Jam");
+        System.out.printf("| %-30s | %-15s | %-10s | %-8s |\n", "Mata Kuliah", "Tempat", "Hari", "Jam");
         System.out.println("---------------------------------------------------------------------");
         for (int i = 0; i < jadwal.length; i++) {
             System.out.printf("| %-30s | %-15s | %-10s | %-8s |\n",
@@ -48,4 +77,51 @@ public class Tugas2 {
         }
         System.out.println("---------------------------------------------------------------------");
     }
+
+    static void tampilkanBerdasarMK(String[][] jadwal, Scanner sc) {
+        sc.nextLine();
+        System.out.print("Masukkan nama Mata Kuliah: ");
+        String cariMK = sc.nextLine();
+
+        boolean ditemukan = false;
+
+        for (int i = 0; i < jadwal.length; i++) {
+            if (jadwal[i][0].equalsIgnoreCase(cariMK)) {
+                System.out.println("\nJadwal ditemukan:");
+                System.out.println("MK     : " + jadwal[i][0]);
+                System.out.println("Tempat : " + jadwal[i][1]);
+                System.out.println("Hari   : " + jadwal[i][2]);
+                System.out.println("Jam    : " + jadwal[i][3]);
+                ditemukan = true;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Jadwal tidak ditemukan.");
+        }
+    }
+
+    static void tampilkanBerdasarHari(String[][] jadwal, Scanner sc) {
+        sc.nextLine();
+        System.out.print("Masukkan hari: ");
+        String cariHari = sc.nextLine();
+
+        boolean ditemukan = false;
+
+        for (int i = 0; i < jadwal.length; i++) {
+            if (jadwal[i][2].equalsIgnoreCase(cariHari)) {
+                System.out.println("\nJadwal ditemukan:");
+                System.out.println("MK     : " + jadwal[i][0]);
+                System.out.println("Tempat : " + jadwal[i][1]);
+                System.out.println("Hari   : " + jadwal[i][2]);
+                System.out.println("Jam    : " + jadwal[i][3]);
+                ditemukan = true;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("Tidak ada jadwal di hari tersebut.");
+        }
+    }
+
 }
