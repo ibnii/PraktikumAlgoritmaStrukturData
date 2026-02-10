430acbda7a7c1c2e71f24ccc18bb2ccceceec0b3
Ibnii <ibniandarta9999@gmail.com>
2026-02-10 08:20:36 +0700

    Jobsheet 1 Perulangan

---
+++ Jobsheet1/PraktikumPerulangan.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PraktikumPerulangan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("NIM\t: ");
+        long nim = sc.nextLong();
+
+        int n = (int) (nim % 100);
+        System.out.println(n);
+
+        if (n < 10) {
+            n += 10;
+        }
+
+        System.out.println("Nilai n = " + n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                System.out.print("# ");
+            } else if (i % 2 == 1) {
+                System.out.print("* ");
+            } else if (i == 10 || i == 15) {
+                continue;
+            } else {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
