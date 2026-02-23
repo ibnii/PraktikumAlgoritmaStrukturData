4df4f541e797587925799013a20ee98587508f7d
Ibnii <ibniandarta9999@gmail.com>
2026-02-24 01:31:56 +0700

    Update 3.4.3 soal 3

--- Jobsheet3/Matakuliah13.java
+++ Jobsheet3/Matakuliah13.java
@@ -12,19 +12,30 @@ public class Matakuliah13 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
-    Matakuliah13(){}
-    void tambahData(){
+
+    Matakuliah13() {
+    }
+
+    void tambahData() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Kode       : ");
-            kode = sc.nextLine();
-            System.out.print("Nama       : ");
-            nama = sc.nextLine();
-            System.out.print("Sks        : ");
-            sks = sc.nextInt();
-            sc.nextLine();
-            System.out.print("Jumlah Jam : ");
-            jumlahJam = sc.nextInt();
-            sc.nextLine();
-            System.out.println("-------------------------------------");
+        kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        nama = sc.nextLine();
+        System.out.print("Sks        : ");
+        sks = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Jumlah Jam : ");
+        jumlahJam = sc.nextInt();
+        sc.nextLine();
+        System.out.println("-------------------------------------");
+    }
+
+    void cetakInfo() {
+        System.out.println("Kode       : " + this.kode);
+        System.out.println("Nama       : " + this.nama);
+        System.out.println("Sks        : " + this.sks);
+        System.out.println("Jumlah Jam : " + this.jumlahJam);
+        System.out.println("-------------------------------------");
     }
 }
