0f0c3f1f1eaea3abfb0fa84396d4d088e41d588b
Ibnii <ibniandarta9999@gmail.com>
2026-02-24 01:19:41 +0700

    Percobaan 3.4

---
+++ Jobsheet3/Matakuliah13.java
@@ -0,0 +1,13 @@
+public class Matakuliah13 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah13(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
