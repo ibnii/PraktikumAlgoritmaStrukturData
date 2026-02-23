3046bfea3bebc003c2f8d9909cc4320e8ab00b92
Ibnii <ibniandarta9999@gmail.com>
2026-02-24 01:34:25 +0700

    Update 3.4.3 soal 4

--- Jobsheet3/MatakuliahDemo13.java
+++ Jobsheet3/MatakuliahDemo13.java
@@ -3,17 +3,18 @@ import java.util.Scanner;
 public class MatakuliahDemo13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah13[] arrayOfMatakuliah = new Matakuliah13[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        System.out.print("Masukkan jumlah elemen : ");
+        int jumlahElemen = sc.nextInt();
+        sc.nextLine();
+        Matakuliah13[] arrayOfMatakuliah = new Matakuliah13[jumlahElemen];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i] = new Matakuliah13();
             arrayOfMatakuliah[i].tambahData();
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             arrayOfMatakuliah[i].cetakInfo();
         }
