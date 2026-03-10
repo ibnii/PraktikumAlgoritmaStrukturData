b9dcbf2c5bf3a814fa8c6b480a1f34d223c5a04e
Ibnii <ibniandarta9999@gmail.com>
2026-03-10 09:12:50 +0700

    Pertanyaan 5.3.3

--- Jobsheet5/Pangkat13.java
+++ Jobsheet5/Pangkat13.java
@@ -6,10 +6,10 @@ public class Pangkat13 {
         pangkat = p;
     }
 
-    int pangkatBF(int a, int n) {
+    int pangkatBF() {
         int hasil = 1;
-        for (int i = 0; i < n; i++) {
-            hasil = hasil * a;
+        for (int i = 0; i < this.pangkat; i++) {
+            hasil = hasil * this.nilai;
         }
         return hasil;
     }
