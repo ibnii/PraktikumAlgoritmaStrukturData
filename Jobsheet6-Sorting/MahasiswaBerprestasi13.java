760571cc2ef134fec687c79df54543a2c9d5dce8
Ibnii <ibniandarta9999@gmail.com>
2026-03-31 07:58:18 +0700

    Praktikum 5.3.2

---
+++ Jobsheet6-Sorting/MahasiswaBerprestasi13.java
@@ -0,0 +1,32 @@
+public class MahasiswaBerprestasi13 {
+    Mahasiswa13[] listMhs = new Mahasiswa13[5];
+    int idx;
+
+    void tambah(Mahasiswa13 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa13 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("------------------------------------");
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                    Mahasiswa13 tmp = listMhs[j];
+                    listMhs[j] = listMhs[j - 1];
+                    listMhs[j - 1] = tmp;
+                }
+            }
+        }
+    }
+}
